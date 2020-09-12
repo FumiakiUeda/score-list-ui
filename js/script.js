36a44b142873371de729f367d7494bd60b84ce78
UmiKazeYu <uosius21@gmail.com>
2020-09-12 14:07:42 +0900

    コメントの整理

--- js/script.js
+++ js/script.js
@@ -52,25 +52,32 @@ function resetForm() {
 }
 
 function tableDownload() {
-    var bom = new Uint8Array([0xEF, 0xBB, 0xBF]);//文字コードをBOM付きUTF-8に指定
-    var table = document.getElementById('scoreTable');//id=scoreTableという要素を取得
-    var data_csv = "";//ここに文字データとして値を格納していく
+    // 文字コードをBOM付きUTF-8に指定
+    var bom = new Uint8Array([0xEF, 0xBB, 0xBF]);
+    // id=scoreTableという要素を取得
+    var table = document.getElementById('scoreTable');
+    // ここに文字データとして値を格納していく
+    var data_csv = "";
 
     for (var i = 0; i < table.rows.length; i++) {
         for (var j = 0; j < table.rows[i].cells.length - 1; j++) {
-            data_csv += table.rows[i].cells[j].innerText;//HTML中の表のセル値をdata_csvに格納
-            if (j == table.rows[i].cells.length - 2) data_csv += "\n";//行終わりに改行コードを追加
-            else data_csv += ",";//セル値の区切り文字として,を追加
+            data_csv += table.rows[i].cells[j].innerText;
+            // 行終わりに改行コードを追加
+            if (j == table.rows[i].cells.length - 2) data_csv += "\n";
+            // セル値の区切り文字として,を追加
+            else data_csv += ",";
         }
     }
 
-    var blob = new Blob([bom, data_csv], { "type": "text/csv" });//data_csvのデータをcsvとしてダウンロードする関数
-    if (window.navigator.msSaveBlob) { //IEの場合の処理
+    var blob = new Blob([bom, data_csv], { "type": "text/csv" });
+    if (window.navigator.msSaveBlob) { // IEの場合の処理
         window.navigator.msSaveBlob(blob, "test.csv");
-        //window.navigator.msSaveOrOpenBlob(blob, "test.csv");// msSaveOrOpenBlobの場合はファイルを保存せずに開ける
+        // window.navigator.msSaveOrOpenBlob(blob, "test.csv");
+        // msSaveOrOpenBlobの場合はファイルを保存せずに開ける
     } else {
         document.getElementById("download").href = window.URL.createObjectURL(blob);
     }
 
-    delete data_csv;//data_csvオブジェクトはもういらないので消去してメモリを開放
+    //data_csvオブジェクトはもういらないので消去してメモリを開放
+    delete data_csv;
 }
