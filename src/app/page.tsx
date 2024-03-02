e2fe3df373efc34b326eba52104d7492263a71c2
UmiKazeYu <uosius21@gmail.com>
2024-03-02 22:58:18 +0900

    add: ページネーションと見出しを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 import { Header } from "@/components/Header";
+import { Heading } from "@/components/Heading";
 import { List } from "@/components/List";
-import Image from "next/image";
+import { Pagenation } from "@/components/Pagenation";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div className="w-full lg:flex">
+      <main className="flex flex-col justify-between px-24 py-5 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <Heading sectionName={'譜面一覧'} />
+        <div className="w-full">
           <List />
+          <Pagenation />
         </div>
       </main>
     </>
